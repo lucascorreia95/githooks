b15980a108041cf7acee7e4d619e5126fcd8aa35
Lucas Correia <lucascorreia-.-@live.com>
2019-04-14 18:54:12 -0300

    react hooks

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+import App2 from './App2'
+
+export default function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect( async () => {
+    const response = await fetch('https://api.github.com/users/lucascorreia95/repos')
+    const data = await response.json()
+
+    setRepositories(data)
+  },[])
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite)
+
+    document.title = `voce tem ${filtered.length} favoritos`
+  }, [repositories])
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map( repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    })
+
+    setRepositories(newRepositories)
+  }
+
+  return (
+    <>
+      <ul>
+        { repositories.map(repo => (
+          <li key={ repo.id }>
+            { repo.name }
+            {repo.favorite && <span>(Favorito)</span>}
+            <button onClick={ () => handleFavorite(repo.id) }>Favoritar</button>
+          </li>
+        )) }
+      </ul>
+
+      <App2 />
+
+    </>
+  )
+}
